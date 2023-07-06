0e9e511d20e9c66f15f0a3cb481500bb837574ac
sachinrajput99 <129744384+sachinrajput99@users.noreply.github.com>
2023-07-06 21:45:20 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.png";
+
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+///////card image
+import nextjsimg from "./nextlogo.jpg";
+import reactlogo from "./reactlogo.jpg";
+import tailwindlogo from "./tailwindlogo.jpg";
+import htmlcssjslogo from "./htmlcssjslogo.jpg";
+
+import css from "./tech/css.png";
+
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+//
+import nextjs from "./tech/nextjs.png";
+
+import retailwagon from "./company/retailwagon.png";
+import freelancer from "./company/freelancer.png";
+
+//projects
+import hoobank from "./hoobank.png";
+import backroads from "./backroads.png";
+import ecommerce from "./ecommerce.png";
+
+export {
+  logo,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  retailwagon,
+  freelancer,
+  ///
+  nextjs,
+  nextjsimg,
+  reactlogo,
+  tailwindlogo,
+  htmlcssjslogo,
+  hoobank,
+  backroads,
+  ecommerce,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,227 @@
+import {
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  mongodb,
+  git,
+  figma,
+  threejs,
+  //
+  nextjsimg,
+  retailwagon,
+  freelancer,
+  nextjs,
+  reactlogo,
+  tailwindlogo,
+  htmlcssjslogo,
+  hoobank,
+  backroads,
+  ecommerce,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Next js Developer",
+    icon: nextjsimg,
+  },
+  {
+    title: "React js Developer",
+    icon: reactlogo,
+  },
+  {
+    title: "Tailwind Developer",
+    icon: tailwindlogo,
+  },
+  {
+    title: "Html/CSS/Javascript Developer",
+    icon: htmlcssjslogo,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Next.js",
+    icon: nextjs,
+  },
+  //////////////////////////////////////////////////////////
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "retailwagon",
+    icon: retailwagon,
+    iconBg: "#383E56",
+    date: "March 2021 - April 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Next.JS Developer",
+    company_name: "Freelancer",
+    icon: freelancer,
+    iconBg: "#E6DEDD",
+    date: "April 2023 - Now",
+    points: [
+      "Developing and maintaining web applications using Next.js and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "  Learning new technologies",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but this person proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients'  like this guy does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After you optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Banking UI/UX",
+    description:
+      " HooBank is the best business landing page. Fully Responsive modern UI/UX website in Reactjs and Tailwind CSS.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: hoobank,
+    source_code_link: "https://bankingsolution.netlify.app",
+  },
+  {
+    name: "tour plan",
+    description:
+      "Web application that enables users to plan their tours with variety of option available to them  .",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: backroads,
+    source_code_link: "https://tourplann.netlify.app",
+  },
+  {
+    name: "ecommerce site",
+    description:
+      "  E-Commerce Project, where I'm building a online store named Comfy Sloth. In this web application we can filter the products, add them to cart , pay online for the product  and order them  ",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "OAuth",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ecommerce,
+    // source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
